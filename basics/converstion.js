9c3eff6d676fa8d916619aed49272117c594a399
Chandraprakash-Pandey <cp.o.pandey@gmail.com>
2024-06-15 17:18:51 +0530

    Conversion of  datatypes

---
+++ basics/converstion.js
@@ -0,0 +1,19 @@
+let score = true; //null 123 undefined
+
+// console.log(typeof score);
+// console.log(score);
+// console.log(typeof (score));
+
+let valeInNum = Number(score);
+// console.log(typeof valeInNum);
+// console.log(valeInNum); //Not a number NaN
+
+let loggedIn = 1; // "" hitesh
+let booleanLoggedIn = Boolean(loggedIn)
+//console.log(typeof booleanLoggedIn);
+//console.log(booleanLoggedIn); //false true
+// 1=> true 0=>false
+
+let someNum = 89;
+let stringNum = String(someNum)
+console.log(stringNum);
--- basics/datatype.js
+++ basics/datatype.js
@@ -1,2 +1,6 @@
 let state = null;
+let city;
 console.log(state);
+
+console.log(typeof state);
+console.log(typeof city);
