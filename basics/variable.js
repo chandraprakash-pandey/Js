c602f848d302e00809cfc735f3aa97ca53dea2f3
Chandraprakash-Pandey <cp.o.pandey@gmail.com>
2024-06-15 16:30:15 +0530

    Learned Basics And data ypes

---
+++ basics/variable.js
@@ -0,0 +1,10 @@
+const accountId = 1234;
+let accoundName = "pandey";
+var accoundPass = "yoo";
+accountCity = "Pune";
+// accountId = 111;
+accoundName = "Chandu"
+accoundPass = "Damn"
+accountCity = "mum"
+console.log(accountId);
+console.table([accountId, accoundName, accoundPass, accountCity]) 
