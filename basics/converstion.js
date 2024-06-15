0fd70438903f5392a24fa70c90e79d26de35572d
Chandraprakash-Pandey <cp.o.pandey@gmail.com>
2024-06-15 22:00:50 +0530

    operations

--- basics/converstion.js
+++ basics/converstion.js
@@ -16,4 +16,25 @@ let booleanLoggedIn = Boolean(loggedIn)
 
 let someNum = 89;
 let stringNum = String(someNum)
-console.log(stringNum);
+// console.log(stringNum);
+
+// Operations
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**2);
+// console.log(2/2);
+
+let str1 = "Hello"
+let str2 = " World"
+
+let str3 = str1+str2
+// console.log(str3);
+
+// console.log(1+2);
+// console.log("1"+2);
+// console.log(1+"2");
+// console.log("1"+2+2);
+// console.log(1+2+"2");
+// console.log(true);
+console.log(+"");
