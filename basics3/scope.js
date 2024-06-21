0598bac223d9e31d57f8641303397b90b56627f8
Chandraprakash-Pandey <cp.o.pandey@gmail.com>
2024-06-21 20:12:39 +0530

    Scope

--- basics3/scope.js
+++ basics3/scope.js
@@ -6,11 +6,40 @@ if(true){
     let a = 100 // block scope
     const b = 200
     // var c = 300
-    console.log(a);
-    console.log(b);
+    // console.log(a);
+    // console.log(b);
     // console.log(c);
 }
 
-console.log(a);
-console.log(b);
+// console.log(a);
+// console.log(b);
 // console.log(c);
+
+function one(){
+    const name = "Chandraprakash"
+
+    function two(){
+        const surname = "Pandey"
+        // console.log(name);
+    }
+    two()
+    // console.log(surname);
+}
+
+one()
+
+
+// ++++++++++++++++++++++++++++++++++++++++++
+addOne(3)
+
+function addOne(num){
+    return num + 1
+}
+
+// addTwo(3) // here it will show error as variable can't be access before it is made
+
+const addTwo = function(num1){
+    return num1 + 2
+}
+
+addTwo(3)
