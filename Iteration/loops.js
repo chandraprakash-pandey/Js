7e42be70ce12a79ea30dc0935cc4681747701c64
Chandraprakash-Pandey <cp.o.pandey@gmail.com>
2024-06-22 23:31:47 +0530

    Learned Loops

---
+++ Iteration/loops.js
@@ -0,0 +1,38 @@
+// for of
+ 
+let myarray = [1,2,3,4,5]
+
+// for (const index of myarray) {
+//     console.log(index);
+// }
+
+const greeting = "hello world!"
+for (const greet of greeting) {
+    // console.log(greet);
+}
+
+// Map
+
+let map = new Map()
+map.set("IN", "Indian")
+map.set("FR", "France")
+map.set("SR", "Shri Lanka")
+map.set("IN", "Indian")
+
+// console.log(map);
+
+// for (const key of map) {
+//     console.log(key);
+// }
+for (const [key, value] of map) {
+    // console.log(`${key} = ${value}`);
+}
+
+let myObj = {
+    name: "Pandey",
+    age: 18
+}
+
+for (const keys of myObj) {
+    console.log(keys);
+}
---
+++ Iteration/while_loop.js
@@ -0,0 +1,19 @@
+let random = 0;
+// while (random <=10) {
+//     console.log(random);
+//     random = random + 2;
+// }
+
+let myArray = ["Noob", "pro", "Legend"]
+let index = 0;
+
+while (index<myArray.length) {
+    // console.log(`Value of index ${index} in array is ${myArray[index]}`);
+    index++;
+}
+
+let score = 11
+do {
+    console.log(`Value of score is ${score}`);
+    score++;
+} while (score<=10);
